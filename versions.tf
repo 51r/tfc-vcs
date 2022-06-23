6404bf2bff22ab8022d4b6f671e07278440d83ea
51r <petar.spasov@hashicorp.com>
2022-06-23 13:31:08 +0300

    removed cloud block

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+/*
+  cloud {
+    organization = "hashicorp-test-peter"
+
+    workspaces {
+      name = "peter-tfc-workspace"
+    }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.19.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
